34b7868572f7b0e8e1f2402650b9b7c62b14d3b2
Brent Williams <wtnerb@gmail.com>
2018-01-22 23:26:54 -0800

    quiz scaffold

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
 'use strict'
+
+var button = getElementById('submit');
+function onClick(event) {
+    if (localStorage.results) alert ('You have already taken the quiz! No cheating!');
+    else {
+        window.location.href('quiz.html');
+    }
+}
+addEventlistener ('submit', onClick);
