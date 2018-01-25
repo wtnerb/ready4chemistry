99928fa4e2771a9c444e35dba519d4d04175ccac
Brent Williams <wtnerb@gmail.com>
2018-01-24 23:07:38 -0800

    cleaning code. refined results display. New category property.

--- quiz.js
+++ quiz.js
@@ -1,29 +1,26 @@
 'use strict';
 
-(function (){
-  let el = document.getElementById('username');
-  el.textContent = localStorage.userName ? localStorage.userName : 'anonymous';
-})();
 
-function Question (content, options, truth){
+function Question (content, options, truth, category){
     this.content = content;
     this.options = options;
     this.truth = truth;
+    this.category = category
     qs.push(this);
 }
 
 Question.prototype.ask = function () {
-    let el = chemifyinHTML('p', this.content);
+    let el = chemifyInHTML('p', this.content);
     el.classList.add('temp','question','text');
     return el;
 }
 
-function chemifyinHTML (tag, str){
+function chemifyInHTML (tag, str){
     str = str.replace (/=>/g, '\u2192').replace(/z(\d+)/g, '<<$1<<').replace(/\^(\d*[+-])/g, '<<$1<<');
     let arr = str.split('<<');
     let newEl = document.createElement(tag);
     for (let i in arr){
-    if (arr[i].match(/^\d*[+-]*$/)) {
+        if (arr[i].match(/^\d*[+-]*$/)) {
             let el = document.createElement('span');
             el.className = arr[i].match(/[+-]/) ? 'superscript' : 'subscript';
             el.textContent = arr[i];
@@ -40,8 +37,7 @@ function Results () {
     this.correct = truth;
 }
 
-function nextQuestion () {
-    let q = qs[iter];
+function nextQuestion (q) {
     let el = document.getElementById('question');
     let old = document.getElementsByClassName('temp');
     while (old[0]) old[0].remove();
@@ -59,7 +55,7 @@ Question.prototype.answers = function () {
         el.id = y;
         el.value = y;
         el.classList.add('temp', 'question', 'checkbox');
-        let ans =  chemifyinHTML('label', x);
+        let ans =  chemifyInHTML('label', x);
         ans.for = y;
         ans.classList.add('temp', 'question', 'answer');
         list.appendChild(el);
@@ -79,24 +75,45 @@ function placeHolder (event) {
     let i = document.getElementById('answers').ans.value;
     result.push ({userAnswer: qs[iter].options[i], time: event.time});
     result[iter].correct = result[iter].userAnswer == qs[iter].truth;
+    result[iter].category = qs[iter].category;
     iter++;
-    if (qs[iter]) nextQuestion();
+    if (qs[iter]) nextQuestion(qs[iter]);
     else {
         let old = document.getElementsByClassName('temp');
         while (old[0]) old[0].remove();
         let el = document.createElement('p');
-        el.textContent = 'Your result is ' + result.filter(x => x.correct).reduce(x => x + 1, 0) + ' correct out of ' + qs.length;
+        el.textContent = 'Your result is ' + result.filter(x => x.correct).reduce(x => x + 1, 0) + ' correct out of ' + qs.length + '. Dont forget that every item on this quiz is fundamental to 163, if you decide to continue your professor will assume you already know and have mastered this material.';
+        document.getElementById('ask').appendChild(el);
+        el = document.createElement('p');
+        el.textContent = 'You might want to work on:';
+        document.getElementById('ask').appendChild(el);
+        el = document.createElement('ul')
+        if (result.filter(x => !x.correct).length == 0) {
+            let e = document.createElement('li');
+            e.textContent = 'Nothing! You are ready to begin!';
+            el.appendChild(e);
+        }
+        result.filter(x => !x.correct).forEach(x => {
+            let e = document.createElement('li');
+            e.textContent = x.category
+            el.appendChild(e);
+        })
         document.getElementById('ask').appendChild(el);
     }
 }
 
 let qs = [];
-new Question ('how old are you?', ['19','22','26','32'], '26');
-new Question ('who are you?', ['bob', 'alice','jessica','nobody'],'nobody');
-new Question ('SOz4^2- is called', ['sulfate', 'everclear', 'poison'], 'sulfate');
 let result = [];
 let iter = 0;
-if (!localStorage.quizStarted) {
-    //localStorage.quizStarted = true; //TODO uncomment when out of dev environment
-    nextQuestion();
-} else alert ('You have already taken the quiz! No retake!');
+(function (){
+    let el = document.getElementById('username');
+    el.textContent = localStorage.userName ? localStorage.userName : 'anonymous';
+    new Question ('how old are you?', ['19','22','26','32'], '26', 'counting');
+    new Question ('who are you?', ['bob', 'alice','jessica','nobody'],'nobody', 'identification');
+    new Question ('SOz4^2- is called', ['sulfate', 'everclear', 'poison'], 'sulfate', 'polyatomic ions');
+    if (!localStorage.quizStarted) {
+        //localStorage.quizStarted = true; //TODO uncomment when out of dev environment
+        nextQuestion(qs[0]);
+    } else {alert ('You have already taken the quiz! No retake!')};
+    
+})();
