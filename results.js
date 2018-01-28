daa309c9d7b376cf2a432b11454f09e0fbdc29a8
Brent Williams <wtnerb@gmail.com>
2018-01-27 18:00:50 -0800

    results page built and functioning. Needs style

--- results.js
+++ results.js
@@ -1,19 +1,28 @@
 'use strict'
 
-let targetTime = 20 * 60;
+const targetTime = 20 * 60;
+const numQuestions = 15;
 let result = JSON.parse(localStorage.result);
+
 
 function results (result){
     let el = document.createElement('p');
-    let numCorrect = result.filter(x => x.correct).reduce(x => x + 1, 0)
+    const numCorrect = result.filter(x => x.correct).reduce(x => x + 1, 0);
     result.forEach(timer);
-    el.textContent = 'You got ' + numCorrect + ' correct out of ' + qs.length // + '. ' + (numCorrect > 17/20) ? '\nDont forget that every item on this quiz is fundamental to 163, if you decide to continue your professor will assume you already know and have mastered this material.' : '\nYou should think very carefully about coninuing to 163. It is not recommended.';
+    el.textContent = 'You got ' + numCorrect + ' correct out of ' + numQuestions;
     document.getElementById('grade').appendChild(el);
-    let t = result.reduce((accum, x) => accum + Math.floor(x.time / 1000), 0)
-    displayLearningOpportunities(result, t);
-    el = document.createElement('p')
-    el.textContent = 'it took you ' + formatDuration(t) + ' to complete the quiz.\nIt should take under ' + timeToComplete + ' minutes.';
-    document.getElementById('ask').appendChild(el)
+    const timeToComplete = result.reduce((accum, x) => accum + Math.floor(x.time / 1000), 0);
+    let summ = document.createElement('h4');
+    const score = numCorrect/numQuestions;
+    if (score > .87) summ.textContent = 'You are good to go!';
+    else if (score > .73) summ.textContent = 'You should do some review work before getting deep into 163.';
+    else if (score > .60) summ.textContent = 'Talk with your professor before attempting 163. A thorough review of 161 and 162 is in order.';
+    else summ.textContent = 'Consider retaking chem 161 and/or 162. Continuing to 163 is not recommended.';
+    el = document.createElement('p');
+    el.textContent = 'it took you ' + formatDuration(timeToComplete) + ' to complete the quiz.\nIt should take under ' + targetTime/60 + ' minutes.';
+    document.getElementById('summary').appendChild(summ);
+    document.getElementById('summary').appendChild(el);
+    displayLearningOpportunities(result, timeToComplete);
 }
 
 function displayLearningOpportunities (result, timeSpent) {
@@ -29,9 +38,9 @@ function displayLearningOpportunities (result, timeSpent) {
     if (result.filter(x => !x.correct).length == 0 && targetTime < timeSpent) {
         addLi ('Nothing! you are ready for Chem 163!', el);
     }
-    result.filter(x => !x.correct).forEach(x => addLi (x.content, el))
+    result.filter(x => !x.correct).forEach(x => addLi (x.category, el))
     if (targetTime >= timeSpent) addLi ('Speed/fluency at solving problems.', el);
-    document.getElementById('ask').appendChild(el);
+    document.getElementById('opportunities').appendChild(el);
 }
 
 function formatDuration (seconds) {
@@ -58,3 +67,10 @@ function formatDuration (seconds) {
         .join(', ')
         .replace(/,( \d+ \w+)$/, ' and$1')
 }
+
+function timer (obj, i) {
+    let initial = result[i - 1] ? result[i - 1].timeStamp : 0;
+    obj.time = obj.timeStamp - initial;
+}
+
+results(result);
